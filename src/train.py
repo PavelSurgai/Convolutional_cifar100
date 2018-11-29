d0af5a95c904e006020e5a16e3ce3a9f9ff435d1
PavelSurgai <45405392+PavelSurgai@users.noreply.github.com>
2018-11-29 14:31:10 +0700

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,83 @@
+
+@author: Pavlan
+"""
+
+import numpy
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.layers import Dropout
+from keras.layers.convolutional import Conv2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+import sys
+
+
+
+# Загружаем данные
+(X_train, y_train), (X_test, y_test) = cifar100.load_data()
+# Размер мини-выборки
+batch_size = 32
+# Количество классов изображений
+nb_classes = 100
+# Количество эпох для обучения
+nb_epoch = 1
+# Размер изображений
+img_rows, img_cols = 32, 32
+
+# Нормализуем данные
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# Преобразуем метки в категории
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+
+
+
+# Создаем последовательную модель
+model = Sequential()
+# Первый сверточный слой
+model.add(Conv2D(32, (3, 3), padding='same',
+                        input_shape=(32, 32, 3), activation='relu'))
+# Второй сверточный слой
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+
+# Слой регуляризации Dropout
+model.add(Dropout(0.25))
+# Третий сверточный слой
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+# Четвертый сверточный слой
+model.add(Conv2D(64, (3, 3), activation='relu'))
+# Слой регуляризации Dropout
+model.add(Dropout(0.25))
+# Слой преобразования данных из 2D представления в плоское
+model.add(Flatten())
+# Полносвязный слой для классификации
+model.add(Dense(512, activation='relu'))
+# Слой регуляризации Dropout
+model.add(Dropout(0.5))
+# Выходной полносвязный слой
+model.add(Dense(nb_classes, activation='softmax'))
+
+
+
+# Задаем параметры оптимизации
+sgd = SGD(lr=0.01)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+# Обучаем модель
+model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              epochs=nb_epoch,
+              validation_split=0.1)
+
+# Оцениваем качество обучения модели на тестовых данных
+scores = model.evaluate(X_test, Y_test)
+print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
+model.save(sys.argv[1])
+print ("model saved")
